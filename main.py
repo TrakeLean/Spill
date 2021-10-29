b515b90cba565010ba36f4a4de39bf9bc4fb71ab
TrakeLean <91937397+TrakeLean@users.noreply.github.com>
2021-10-29 03:35:24 +0200

    Update main.py
    Fixed animation and image flip.
    (also added more debug info on screen)

--- main.py
+++ main.py
@@ -34,13 +34,16 @@ screen = pygame.display.set_mode((screenW,screenH))
 clock = pygame.time.Clock()
 pos_y = screenH/2
 pos_x = screenW/2
-model_size = [64,120]
+model_size = [64*0.75,120*0.75]
 pos_vel = 10
-jump_vel = 3
+velocity = 0
+dash_vel = pos_vel*1.5
 walkCount = 0
 walk_ = False
 idle_ = False
 hit_ = False
+left_ = False
+right = False
 
 
 
@@ -56,14 +59,36 @@ pygame.display.set_icon(icon)
 myFont = pygame.font.SysFont("Times New Roman", 18)
 
 def debug():
+  
+  # Animation status
   anim_text = myFont.render('Active Animation: ', True, black)
   anim_value = myFont.render(anim_stat, True, black)
-  rect = anim_text.get_rect()
-  rect.topleft = (20,20)
-
+  rect_anim = anim_text.get_rect()
+  rect_anim.topleft = (20,20)
   
-  screen.blit(anim_text,rect)
-  screen.blit(anim_value,(rect[0]+140,rect[1]))
+  screen.blit(anim_text,rect_anim)
+  screen.blit(anim_value,(rect_anim[0]+140,rect_anim[1]))
+  
+  # Velocity
+  speed = myFont.render('{}'.format(abs(velocity)), True, black)
+  rect_speed = speed.get_rect()
+  rect_speed.topleft = (20,40)
+  
+  screen.blit(speed,rect_speed)
+  
+  # Coordinates
+  # x
+  coord_x_text = myFont.render('X: ', True, black)
+  coord_x = myFont.render('{}'.format(pos_x), True, black)
+  rect_coord_x = coord_x_text.get_rect()
+  rect_coord_x.topleft = (20,60)
+  screen.blit(coord_x_text,rect_coord_x)
+  screen.blit(coord_x,(rect_coord_x[0]*2,rect_coord_x[1]))
+  # y
+  coord_y_text = myFont.render('Y: ', True, black)
+  coord_y = myFont.render('{}'.format(pos_y), True, black)
+  screen.blit(coord_y_text,(rect_coord_x[0],rect_coord_x[1]+20))
+  screen.blit(coord_y,(rect_coord_x[0]*2,rect_coord_x[1]+20))
 
 
 def redrawGameWindow(walk,idle,hit):
@@ -75,6 +100,8 @@ def redrawGameWindow(walk,idle,hit):
     
   if walk_ == True:
     walk = pygame.transform.scale(walk[walkCount//3],(model_size[0],model_size[1]))
+    if left_ == True:
+      walk = pygame.transform.flip(walk,True,False)
     screen.blit(walk,(pos_x,pos_y))
     walkCount += 1
     anim_stat = "Walking"
@@ -82,6 +109,8 @@ def redrawGameWindow(walk,idle,hit):
     
   if idle_ == True:
     idle = pygame.transform.scale(idle[walkCount//3],(model_size[0],model_size[1]))
+    if left_ == True:
+      idle = pygame.transform.flip(idle,True,False)
     screen.blit(idle,(pos_x,pos_y))
     walkCount += 1
     anim_stat = "Idle"
@@ -103,7 +132,7 @@ def redrawGameWindow(walk,idle,hit):
 running = True
 while running:
   
-  clock.tick(30)
+  clock.tick(20)
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
@@ -111,40 +140,55 @@ while running:
       pygame.quit()
 
   keys = pygame.key.get_pressed()
-  if keys[pygame.K_a] and pos_x > pos_vel-11:
-    idle_ = False
-    walk_ = True
+  if keys[pygame.K_a] and pos_x > pos_vel-11:  
+    pos_x_temp = pos_x
     pos_x = pos_x - pos_vel
+    (velocity) = pos_x - pos_x_temp
+    right = False
+    left_ = True
     if keys[pygame.K_SPACE]:
-      pos_x -= jump_vel
+      pos_x_temp = pos_x
+      pos_x -= dash_vel
+      velocity = pos_x - pos_x_temp
 
-
-  if keys[pygame.K_d]and pos_x < screenW-model_size[0]:
-    idle_ = False
-    walk_ = True
+  if keys[pygame.K_d] and pos_x < screenW-model_size[0]:
+    pos_x_temp = pos_x
     pos_x = pos_x + pos_vel
+    velocity = pos_x - pos_x_temp
+    left_ = False
+    right = True
     if keys[pygame.K_SPACE]:
-      pos_x += jump_vel
+      pos_x_temp = pos_x
+      pos_x += dash_vel
+      velocity = pos_x - pos_x_temp
 
                 
-  if keys[pygame.K_w]and pos_y > pos_vel-44:
-    idle_ = False
-    walk_ = True
+  if keys[pygame.K_w] and pos_y > pos_vel-44:
+    pos_y_temp = pos_y
     pos_y = pos_y - pos_vel
+    velocity = pos_y - pos_y_temp
     if keys[pygame.K_SPACE]:
-      pos_y -= jump_vel    
+      pos_y_temp = pos_y
+      pos_y -= dash_vel
+      velocity = pos_y - pos_y_temp
 
     
-  if keys[pygame.K_s]and pos_y < screenH-model_size[1]:
+  if keys[pygame.K_s] and pos_y < screenH-model_size[1]:
+    pos_y_temp = pos_y
+    pos_y = pos_y + pos_vel
+    velocity = pos_y - pos_y_temp
+    if keys[pygame.K_SPACE]:
+      pos_y_temp = pos_y
+      pos_y += dash_vel
+      velocity = pos_y - pos_y_temp
+      
+  if keys[pygame.K_a] or keys[pygame.K_d] or keys[pygame.K_w] or keys[pygame.K_s]:
     idle_ = False
     walk_ = True
-    pos_y = pos_y + pos_vel
-    if keys[pygame.K_SPACE]:
-      pos_y += jump_vel
   else:
-    walkCount += 1
     walk_ = False
     idle_ = True
+    velocity = 0
     
   redrawGameWindow(walk,idle,hit)
 
